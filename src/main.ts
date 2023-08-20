4fb67009d4612f36a99b4528cc9b65299bfc691a
NguyenHoangHieu22GIT2 <133379564+NguyenHoangHieu22GIT2@users.noreply.github.com>
2023-08-20 17:03:14 +0700

    Configure Cors

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,10 @@ import { AppModule } from './app.module';
 import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { json } from 'express';
-import { faker } from '@faker-js/faker';
+import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors({
-    origin: ['https://backend-nguyenhoanghieu1.vercel.app/'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  app.use(cors());
 
   app.use(json({ limit: '50mb' }));
   app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }));
